990ec0251d939c2da968d7edea3da340302e0c21
galoistom <liug17969@gmail.com>
2026-08-11 13:45:55 +0800

    improve the user interface

--- main.go
+++ main.go
@@ -25,13 +25,13 @@ func Process(code string) (Expr, error) {
 	return res, nil
 }
 
-func ReadFile(path string) (string, error){
+func ReadFile(path string) (string, error) {
 	file, err := os.ReadFile(path)
 	if err != nil {
 		fmt.Println(err)
 		return "", err
 	}
-	return string(file),nil
+	return string(file), nil
 }
 
 func main() {
@@ -39,12 +39,17 @@ func main() {
 	var err error
 	switch os.Args[1] {
 	case "--help", "-h":
-		fmt.Println("usage: calculator [option] <code>\n      -f/--file to read code from file\n      -e/--eval to read from arguments")
+		fmt.Println("usage: calculator [option] <code>\n",
+			"        -f/--file to read code from file\n",
+			"        -e/--eval to read from arguments")
 		return
 	case "--file", "-f":
 		filepath := os.Args[2]
-		input,err=ReadFile(filepath)
-		if err!=nil{fmt.Println(err);return}
+		input, err = ReadFile(filepath)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	case "--eval", "-e":
 		input = os.Args[2]
 	}
